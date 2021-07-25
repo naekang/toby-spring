fba2e06ed5838b6e331a186d52f8b0ed7f21b36a
naekang <66901361+naekang@users.noreply.github.com>
2021-07-25 15:25:48 +0900

    XML을 이용하는 ApplicationContext

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -2,19 +2,21 @@ package springbook.user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        ApplicationContext ac = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = ac.getBean("userDao", UserDao.class);
+
         User user = new User();
         user.setId("naekang");
         user.setName("김진호");
         user.setPassword("haul1!");
-
-        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = ac.getBean("userDao", UserDao.class);
 
         System.out.println(user.getId() + "등록 성공");
 
