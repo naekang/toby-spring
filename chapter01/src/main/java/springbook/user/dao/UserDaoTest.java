b51fd4b435303c1bfc43e0f1cf135d7fef741c34
naekang <66901361+naekang@users.noreply.github.com>
2021-07-26 23:50:54 +0900

    change directory & UserDaoTest 다시보기

--- chapter01/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chapter01/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,7 +1,6 @@
 package springbook.user.dao;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
