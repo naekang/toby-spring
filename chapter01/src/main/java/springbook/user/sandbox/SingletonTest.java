4a56a817eee332e064b015896c18169d3b2ec5ce
naekang <66901361+naekang@users.noreply.github.com>
2021-07-31 21:20:32 +0900

    UserDaoTest의 개선

--- chapter01/src/main/java/springbook/user/sandbox/SingletonTest.java
+++ chapter01/src/main/java/springbook/user/sandbox/SingletonTest.java
@@ -1,15 +1,19 @@
-package springbook.user.sandbox;
+package springbook.sandbox;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void testSingletonSpringContext(){
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
-        System.out.println(context.getBean("userDao", UserDao.class));
-        System.out.println(context.getBean("userDao", UserDao.class));
+        assertThat(context.getBean("userDao", UserDao.class)).isEqualTo(context.getBean("userDao", UserDao.class));
     }
 }
