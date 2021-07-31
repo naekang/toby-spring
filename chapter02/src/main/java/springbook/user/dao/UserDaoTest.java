45eec18b98c8a4e7c725775753e7d100ffa15399
naekang <66901361+naekang@users.noreply.github.com>
2021-07-31 17:38:11 +0900

    chapter2 first setting

---
+++ chapter02/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,31 @@
+package springbook.user.dao;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        ApplicationContext ac = new GenericXmlApplicationContext("/applicationContext.xml");
+
+        UserDao userDao = ac.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("naekang");
+        user.setName("김진호");
+        user.setPassword("haul1!");
+        userDao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공");
+    }
+}
