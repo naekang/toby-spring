d4dc77446f425d70f5fcc6712d96fb67d77b9f1c
naekang <66901361+naekang@users.noreply.github.com>
2021-08-01 15:07:45 +0900

    테스트가 이끄는 개발

--- chapter02/src/test/java/springbook/user/dao/UserDaoTest.java
+++ chapter02/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -7,25 +7,35 @@ import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 
 class UserDaoTest {
 
     @Test
-    public void main() throws SQLException {
+    public void addAndGet() throws SQLException {
         ApplicationContext ac = new GenericXmlApplicationContext("/applicationContext.xml");
         UserDao userDao = ac.getBean("userDao", UserDao.class);
 
-        User user = new User();
-        user.setId("nakenag");
-        user.setName("김진호");
-        user.setPassword("haul1!");
-        userDao.add(user);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
 
-        User user2 = userDao.get(user.getId());
+        User user1 = new User("pilhwankim", "김필환", "secret2@");
+        User user2 = new User("leegm700", "이길원", "springno2");
+        userDao.add(user1);
+        userDao.add(user2);
 
-        assertThat(user2.getName()).isEqualTo(user.getName());
-        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+
+        User userget1 = userDao.get(user1.getId());
+
+        assertThat(userget1.getName()).isEqualTo(user1.getName());
+        assertThat(userget1.getPassword()).isEqualTo(user1.getPassword());
+
+        User userget2 = userDao.get(user2.getId());
+        assertThat(userget2.getName()).isEqualTo(user2.getName());
+        assertThat(userget2.getPassword()).isEqualTo(user2.getPassword());
     }
 
 }
